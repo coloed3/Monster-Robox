4fde2e80a537fc8a6e13f674bba9a6a110bbb9ca
Eddie Colon <coloed3@gmail.com>
2019-07-20 23:31:08 -0600

    Updates

---
+++ precache-manifest.02d9e535fed905e94226032a7effa43d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9e8e027cfeed9c6fdf43e7bbe09750",
+    "url": "/Monster-Robox/index.html"
+  },
+  {
+    "revision": "81d74058b1a4cdf33029",
+    "url": "/Monster-Robox/static/css/main.766c3e50.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Robox/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "81d74058b1a4cdf33029",
+    "url": "/Monster-Robox/static/js/main.84bc7641.chunk.js"
+  },
+  {
+    "revision": "d0c09fad5109cdff9bfc",
+    "url": "/Monster-Robox/static/js/runtime~main.d8479cea.js"
+  }
+]);
